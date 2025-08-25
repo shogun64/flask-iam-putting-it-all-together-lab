3cabc67d407fa89e24d14fa37b916d8cf9a68631
Ian Synge <gamersynge@gmail.com>
2025-08-25 16:03:29 -0400

    Updated app.py and models.py

--- server/app.py
+++ server/app.py
@@ -8,19 +8,96 @@ from config import app, db, api
 from models import User, Recipe, UserSchema, RecipeSchema
 
 class Signup(Resource):
-    pass
+    def post(self):
+        json = request.get_json()
+
+        if not json.get('username') or not json.get('password'):
+            return {"errors": "Username and password required"}, 422
+
+        user = User(username=json.get('username'), image_url=json.get('image_url'),
+                    bio=json.get('bio'))
+        user.password_hash = json.get('password')
+        
+        try:
+            db.session.add(user)
+            db.session.commit()
+            session['user_id'] = user.id
+        except IntegrityError:
+            db.session.rollback()
+            return {'errors': ["Username must be unique."]}, 422
+        except ValueError as e:
+            db.session.rollback()
+            return {'errors': [str(e)]}, 422
+        return UserSchema().dump(user), 201
 
 class CheckSession(Resource):
-    pass
+    def get(self):
+        user_id = session.get('user_id')
+        if user_id:
+            user = User.query.get(user_id)
+            if user:
+                return UserSchema().dump(user), 200
+        return {'error': 'Unauthorized'}, 401
 
 class Login(Resource):
-    pass
+    def post(self):
+        data = request.get_json()
+        username = data.get('username')
+        password = data.get('password')
+        if not username or not password:
+            return {'error': 'Username and password required.'}, 401
+
+        user = User.query.filter_by(username=username).first()
+        if user and user.authenticate(password):
+            session['user_id'] = user.id
+            return UserSchema().dump(user), 200
+        else:
+            return {'error': 'Invalid username or password.'}, 401
 
 class Logout(Resource):
-    pass
+    def delete(self):
+        if session.get('user_id'):
+            session.pop('user_id', None)
+            return '', 204
+        else:
+            return {'error': 'Unauthorized'}, 401
 
 class RecipeIndex(Resource):
-    pass
+    def get(self):
+        if not session.get('user_id'):
+            return {'error': 'Unauthorized'}, 401
+        recipes = Recipe.query.all()
+        return RecipeSchema(many=True).dump(recipes), 200
+    def post(self):
+        if not session.get('user_id'):
+            return {'error': 'Unauthorized'}, 401
+
+        data = request.get_json()
+
+        title = data.get('title')
+        instructions = data.get('instructions')
+        minutes = data.get('minutes_to_complete')
+
+        if not title or not instructions:
+            return {'errors': 'Title and instructions required'}, 422
+        elif len(instructions) < 50:
+            return {'errors': 'Instructions must be 50 characters or more'}, 422
+
+        user_id = session.get('user_id')
+        recipe = Recipe(
+            title=title,
+            instructions=instructions,
+            minutes_to_complete=minutes,
+            user_id=user_id
+        )
+        try:
+            db.session.add(recipe)
+            db.session.commit()
+        except Exception as e:
+            db.session.rollback()
+            return {'errors': [str(e)]}, 422
+
+        return RecipeSchema().dump(recipe), 201
 
 api.add_resource(Signup, '/signup', endpoint='signup')
 api.add_resource(CheckSession, '/check_session', endpoint='check_session')
--- server/models.py
+++ server/models.py
@@ -7,15 +7,68 @@ from config import db, bcrypt
 class User(db.Model):
     __tablename__ = 'users'
 
-    pass
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String, unique=True, nullable=False)
+    _password_hash = db.Column(db.String, nullable=True)
+    image_url = db.Column(db.String)
+    bio = db.Column(db.String)
+
+    recipes = db.relationship('Recipe', back_populates='user', 
+                    cascade='all, delete-orphan', lazy='joined')
+
+    @hybrid_property
+    def password_hash(self):
+        raise AttributeError("Password hashes may not be viewed.")
+    
+    @password_hash.setter
+    def password_hash(self, password):
+        password_hash = bcrypt.generate_password_hash(
+        password.encode('utf-8'))
+        self._password_hash = password_hash.decode('utf-8')
+
+    def authenticate(self, password):
+        return bcrypt.check_password_hash(self._password_hash, password.encode('utf-8'))
+
+    @validates('username')
+    def validate_username(self, key, username):
+        if not username:
+            raise ValueError("Username must be present")
+        return username
 
 class Recipe(db.Model):
     __tablename__ = 'recipes'
     
-    pass
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String, nullable=False)
+    instructions = db.Column(db.String, nullable=False)
+    minutes_to_complete = db.Column(db.Integer)
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=True)
+
+    user = db.relationship('User', back_populates='recipes')
+
+    @validates('title')
+    def validate_title(self, key, title):
+        if not title:
+            raise ValueError("Title must not be empty")
+        return title
+    
+    @validates('instructions')
+    def validate_instructions(self, key, instructions):
+        if not instructions or len(instructions) < 50:
+            raise ValueError(
+                "Instructions must be at least 50 characters long")
+        return instructions
 
 class UserSchema(Schema):
-    pass
+    id = fields.Int()
+    username = fields.Str()
+    image_url = fields.Str()
+    bio = fields.Str()
+    recipes = fields.List(fields.Nested(lambda: RecipeSchema(exclude=("user",))))
 
 class RecipeSchema(Schema):
-    pass
+    id = fields.Int()
+    title = fields.Str()
+    instructions = fields.Str()
+    minutes_to_complete = fields.Int()
+    user = fields.Nested(lambda: UserSchema(only=("id", "username", "image_url", "bio")))
